4d90c9184ebfa43b1e32d0c32fbf69070034fac9
Willian Keller <will_levinski@hotmail.com>
2015-09-15 14:45:42 -0300

    1.0.3
    - Cached content

--- core/Compress.php
+++ core/Compress.php
@@ -18,209 +18,263 @@
  */
 
 class Compress {
+    /*
+     * Define cache life time (seconds)
+     * @default 604800
+     * @var $cache (int)
+     */
 
-  public function addStyleSheet($data = array()) {
+    public $cache = 604800;
 
-    $this->type = 'css';
+    public function addStyleSheet($data = array()) {
 
-    return $this->checkData($data);
-  }
+        $this->type = 'css';
 
-  public function addJavaScript($data = array()) {
-
-    $this->type = 'js';
-
-    return $this->checkData($data);
-  }
-
-  protected function checkData($data) {
-
-    if (isset($data['ensemble'])) {
-
-      return $this->filesEnsemble($data);
-    } else if (isset($data['inherent'])) {
-
-      return $this->filesInherent($data);
-    } else {
-      return $this->filesDefalt($data);
-    }
-  }
-
-  private function filesEnsemble($data) {
-
-    return $this->fileCallOut($this->fileWrite($data));
-  }
-
-  private function filesInherent($data) {
-
-    $content = $this->arrayContent($data['includes']);
-
-    if (isset($data['compress'])) {
-
-      $content = $this->filesCompress($content);
+        return $this->checkData($data);
     }
 
-    return $this->fileCallOut($content, true);
-  }
+    public function addJavaScript($data = array()) {
 
-  private function filesDefalt($data) {
+        $this->type = 'js';
 
-    foreach ($data['includes'] as $file) {
-
-      $content[] = $this->fileCallOut($this->_folder($file, true));
+        return $this->checkData($data);
     }
 
-    return implode(PHP_EOL, $content);
-  }
+    protected function checkData($data) {
 
-  private function filesCompress($data) {
+        if (isset($data['ensemble'])) {
 
-    return $this->compressContent($data);
-  }
+            return $this->filesEnsemble($data);
+        } else if (isset($data['inherent'])) {
 
-  private function fileCallOut($content, $method = null) {
-
-    if ($method === true) {
-
-      switch ($this->type) {
-
-        case 'css':
-          return '<style>' . $content . '</style>';
-        case 'js':
-          return '<script>' . $content . '</script>';
-        default :
-          return $content;
-      }
+            return $this->filesInherent($data);
+        } else {
+            return $this->filesDefalt($data);
+        }
     }
-    switch ($this->type) {
 
-      case 'css':
-        return '<link rel="stylesheet" type="text/css" href="' . $content . '"/>';
-      case 'js':
-        return '<script src="' . $content . '"></script>';
-      default :
-        return $content;
+    private function filesEnsemble($data) {
+
+        return $this->fileCallOut($this->fileWrite($data));
     }
-  }
 
-  /*
-   * Function read file
-   * @param varchar $file
-   * @returns varchar $file content
-   */
+    private function filesInherent($data) {
 
-  private function fileRead($file) {
+        $content = $this->arrayContent($data['includes']);
+
+        if (isset($data['compress'])) {
+
+            $content = $this->filesCompress($content);
+        }
+
+        return $this->fileCallOut($content, true);
+    }
+
+    private function filesDefalt($data) {
+
+        foreach ($data['includes'] as $file) {
+
+            $content[] = $this->fileCallOut($this->_folder($file, true));
+        }
+
+        return implode(PHP_EOL, $content);
+    }
+
+    private function filesCompress($data) {
+
+        return $this->compressContent($data);
+    }
+
+    private function fileCallOut($content, $method = null) {
+
+        if ($method === true) {
+
+            switch ($this->type) {
+
+                case 'css':
+                    return '<style>' . $content . '</style>';
+                case 'js':
+                    return '<script>' . $content . '</script>';
+                default :
+                    return $content;
+            }
+        }
+        switch ($this->type) {
+
+            case 'css':
+                return '<link rel="stylesheet" type="text/css" href="' . $content . '"/>';
+            case 'js':
+                return '<script src="' . $content . '"></script>';
+            default :
+                return $content;
+        }
+    }
 
     /*
-     * Open file content
+     * Function read file
+     * @param varchar $file
+     * @returns varchar $file content
      */
-    $fopen = fopen($this->_folder($file), 'r');
 
-    return fread($fopen, filesize($this->_folder($file)));
-  }
+    private function fileRead($file) {
 
-  private function fileWrite($data) {
+        /*
+         * Open file content
+         */
+        $fopen = fopen($this->_folder($file), 'r');
 
-    $content = $this->arrayContent($data['includes']);
-
-    $filename = 'assets/' . $this->type . '/min/' . md5(date('Y-m')) . '.' . $this->type;
-
-    $myfile = fopen($this->_folder($filename), "w") or die("Unable to open file!");
-
-    if (isset($data['compress'])) {
-
-      fwrite($myfile, $this->filesCompress($content));
-    } else {
-
-      fwrite($myfile, $content);
-    }
-    fclose($myfile);
-
-    return $this->_folder($filename, true);
-  }
-
-  private function arrayContent($files) {
-
-    foreach ($files as $file) {
-
-      $content[] = $this->fileRead($file);
+        return fread($fopen, filesize($this->_folder($file)));
     }
 
-    return implode(PHP_EOL, $content);
-  }
+    private function fileWrite($data) {
 
-  private function _folder($arquivo, $method = null) {
+        $content = $this->arrayContent($data['includes']);
 
-    // Retorna a rota base do arquivo com a extenção
-    return (($method != null) ? URL : APP_PATH) . $arquivo;
-  }
+        $filename = 'assets/' . $this->type . '/min/' . md5(date('Y-m')) . '.' . $this->type;
 
-  /**
-   * _compressContent()
-   * Função responsável por remover os espaços desnecessários entre os caracteres
-   *
-   * @function private
-   * @return void
-   */
-  private function compressContent($content) {
+        if (!file_exists($this->_folder($filename))) {
 
-    $this->content = $content;
+            $myfile = fopen($this->_folder($filename), "w") or die("Unable to open file!");
+            
+            $cached = $this->fileCache($filename);
 
-    $this->content = $this->_removeComments($this->content);
+            if (isset($data['compress'])) {
 
-    $this->content = $this->_removeAllSpaces($this->content);
+                fwrite($myfile, $cached . $this->filesCompress($content));
+            } else {
 
-    $this->content = $this->_removeCharSpaces($this->content);
+                fwrite($myfile, $cached . $content);
+            }
+            fclose($myfile);
+        }
 
-    return $this->content;
-  }
+        return $this->_folder($filename, true);
+    }
 
-  private function _removeCharSpaces($content) {
+    private function arrayContent($files) {
 
-    $this->content = $content;
+        foreach ($files as $file) {
 
-    $this->content = str_replace(array(" {", "{ "), '{', $this->content);
-    $this->content = str_replace(array(" }", "} "), '}', $this->content);
-    $this->content = str_replace(array(' <', '< '), '<', $this->content);
-    $this->content = str_replace(array(' >', '> '), '>', $this->content);
-    $this->content = str_replace(array(' +', '+ '), '+', $this->content);
-    $this->content = str_replace(array(' -', '- '), '-', $this->content);
-    $this->content = str_replace(array(' ]', '] '), ']', $this->content);
-    $this->content = str_replace(array(' [', '[ '), '[', $this->content);
-    $this->content = str_replace(array(';}', '} '), '}', $this->content);
-    $this->content = str_replace(array(' ;', '; '), ';', $this->content);
-    $this->content = str_replace(array(' (', '( '), '(', $this->content);
-    $this->content = str_replace(array(' )', ') '), ')', $this->content);
-    $this->content = str_replace(array(' ,', ', '), ',', $this->content);
-    $this->content = str_replace(array(' :', ': '), ':', $this->content);
+            $content[] = $this->fileRead($file);
+        }
 
-    return $this->content;
-  }
+        return implode(PHP_EOL, $content);
+    }
 
-  private function _removeAllSpaces($content) {
+    private function fileCache($filename) {
 
-    $this->content = $content;
+        $lasts = $this->_lifetime($filename);
 
-    $this->content = str_replace(array("\t", "\n", "\r", '  ', '    ', '     '), '', $this->content);
-    $this->content = str_replace(array(' =', '= '), '=', $this->content);
-    $this->content = str_replace(array(' ==', '== '), '==', $this->content);
-    $this->content = str_replace(array(' &&', '&& '), '&&', $this->content);
-    $this->content = str_replace(array(' ||', '|| '), '||', $this->content);
-    $this->content = str_replace(array(' !==', '!== '), '!==', $this->content);
-    $this->content = str_replace(array(' ===', '=== '), '===', $this->content);
+        //get the last-modified-date of this very file
+        $lastModified = $lasts;
 
-    return $this->content;
-  }
+        //get a unique hash of this file (etag)
+        $etagFile = md5_file($filename);
 
-  private function _removeComments($content) {
+        //get the HTTP_IF_MODIFIED_SINCE header if set
+        $ifModifiedSince = (isset($_SERVER['HTTP_IF_MODIFIED_SINCE']) ? $_SERVER['HTTP_IF_MODIFIED_SINCE'] : false);
 
-    $this->content = $content;
+        //get the HTTP_IF_NONE_MATCH header if set (etag: unique file hash)
+        $etagHeader = (isset($_SERVER['HTTP_IF_NONE_MATCH']) ? trim($_SERVER['HTTP_IF_NONE_MATCH']) : false);
 
-    $this->content = preg_replace('!/\*.*?\*/!s', '', $this->content);
-    $this->content = preg_replace('/\n\s*\n/', "\n", $this->content);
-    $this->content = preg_replace('/((?:\/\*(?:[^*]|(?:\*+[^*\/]))*\*+\/)|(?:\/\/.*))/', '', $this->content);
+        //set last-modified header
+        header("Last-Modified: " . gmdate("D, d M Y H:i:s", $lastModified) . " GMT");
 
-    return $this->content;
-  }
+        //set etag-header
+        header("Etag: $etagFile");
+
+        //make sure caching is turned on
+        header('Cache-Control: public');
+
+        //check if page has changed. If not, send 304 and exit
+        if (strtotime($ifModifiedSince) == $lastModified || $etagHeader == $etagFile) {
+
+            header("HTTP/1.1 304 Not Modified");
+            header('Cache-Control:');
+            exit;
+        }
+
+        return '/* This file was last modified: ' . date("d.m.Y H:i:s", time()) . '*/';
+    }
+
+    private function _lifetime($file) {
+
+        // Monta o laço dos arquivos
+        return filemtime($this->_folder($file));
+    }
+
+    private function _folder($arquivo, $method = null) {
+
+        // Retorna a rota base do arquivo com a extenção
+        return (($method != null) ? URL : APP_PATH) . $arquivo;
+    }
+
+    /**
+     * _compressContent()
+     * Função responsável por remover os espaços desnecessários entre os caracteres
+     *
+     * @function private
+     * @return void
+     */
+    private function compressContent($content) {
+
+        $this->content = $content;
+
+        $this->content = $this->_removeComments($this->content);
+
+        $this->content = $this->_removeAllSpaces($this->content);
+
+        $this->content = $this->_removeCharSpaces($this->content);
+
+        return $this->content;
+    }
+
+    private function _removeCharSpaces($content) {
+
+        $this->content = $content;
+
+        $this->content = str_replace(array(" {", "{ "), '{', $this->content);
+        $this->content = str_replace(array(" }", "} "), '}', $this->content);
+        $this->content = str_replace(array(' <', '< '), '<', $this->content);
+        $this->content = str_replace(array(' >', '> '), '>', $this->content);
+        $this->content = str_replace(array(' +', '+ '), '+', $this->content);
+        $this->content = str_replace(array(' -', '- '), '-', $this->content);
+        $this->content = str_replace(array(' ]', '] '), ']', $this->content);
+        $this->content = str_replace(array(' [', '[ '), '[', $this->content);
+        $this->content = str_replace(array(';}', '} '), '}', $this->content);
+        $this->content = str_replace(array(' ;', '; '), ';', $this->content);
+        $this->content = str_replace(array(' (', '( '), '(', $this->content);
+        $this->content = str_replace(array(' )', ') '), ')', $this->content);
+        $this->content = str_replace(array(' ,', ', '), ',', $this->content);
+        $this->content = str_replace(array(' :', ': '), ':', $this->content);
+
+        return $this->content;
+    }
+
+    private function _removeAllSpaces($content) {
+
+        $this->content = $content;
+
+        $this->content = str_replace(array("\t", "\n", "\r", '  ', '    ', '     '), '', $this->content);
+        $this->content = str_replace(array(' =', '= '), '=', $this->content);
+        $this->content = str_replace(array(' ==', '== '), '==', $this->content);
+        $this->content = str_replace(array(' &&', '&& '), '&&', $this->content);
+        $this->content = str_replace(array(' ||', '|| '), '||', $this->content);
+        $this->content = str_replace(array(' !==', '!== '), '!==', $this->content);
+        $this->content = str_replace(array(' ===', '=== '), '===', $this->content);
+
+        return $this->content;
+    }
+
+    private function _removeComments($content) {
+
+        $this->content = $content;
+
+        $this->content = preg_replace('!/\*.*?\*/!s', '', $this->content);
+        $this->content = preg_replace('/\n\s*\n/', "\n", $this->content);
+        $this->content = preg_replace('/((?:\/\*(?:[^*]|(?:\*+[^*\/]))*\*+\/)|(?:\/\/.*))/', '', $this->content);
+
+        return $this->content;
+    }
 
 }
